6ba0d52d3e25bb30361a36447de1f15196c15e58
EROL AKGÜL <erolakgul88@gmail.com>
2021-11-17 12:24:14 +0300

    Add project files.

---
+++ RabbitMQ_NewTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ_NewTask.Service;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ_NewTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                    channel.QueueDeclare(queue: "task_queue",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                    Thread thr1 = new Thread(() => Sender.GetData(channel));
+                    thr1.Start();
+
+                    Console.ReadLine();
+                }
+            }
+        }
+
+    }
+
+
+}
---
+++ RabbitMQ_NewTask/Service/Sender.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQ_NewTask.Service
+{
+    public static class Sender
+    {
+        public static void GetData(IModel channel)
+        {
+            string[] _arr = { ".", "..", "...", "....", "...." };
+            string message = String.Empty;
+
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                message = i + ". Message" + _arr[i];
+
+                Console.WriteLine(message);
+                Thread.Sleep(i * 1000);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "task_queue",
+                                     basicProperties: properties,
+                                     body: body);
+
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+    }
+}
